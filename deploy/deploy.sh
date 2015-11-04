1949c38d14bedb44d4c520c96068cd9293153acf
Shudi Wang <shudiwsh2009@gmail.com>
2015-11-04 14:30:22 +0800

    add deployment file

---
+++ deploy/deploy.sh
@@ -0,0 +1,30 @@
+echo "#update git"
+cd /mnt/appointment/git/Appointment/
+echo "git fetch..."
+git checkout master
+git fetch --all
+echo "git update..."
+git reset --hard origin/master
+git pull origin master
+
+echo "#set system env"
+source /root/.bash_profile
+
+echo "#deploy war"
+echo "shutdown tomcat..."
+/mnt/appointment/apache-tomcat-8.0.26/bin/shutdown.sh
+sleep 5
+echo "replace war..."
+rm -rf /mnt/appointment/apache-tomcat-8.0.26/webapps/appointment*
+cp /mnt/appointment/git/Appointment/target/appointment.war /mnt/appointment/apache-tomcat-8.0.26/webapps
+echo "start tomcat..."
+/mnt/appointment/apache-tomcat-8.0.26/bin/startup.sh
+sleep 10
+
+echo "#deploy jar"
+echo "replace jar..."
+cp -f /mnt/appointment/git/Appointment/target/appointment.jar /mnt/appointment/reminder
+echo "restart cron"
+service cron restart
+
+echo "###Deployment Completed###"
---
+++ deploy/reminder.sh
@@ -0,0 +1,2 @@
+source /root/.bash_profile
+java -jar /mnt/appointment/reminder/appointment.jar
