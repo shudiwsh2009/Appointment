44f8876b3dcad2360e938fb42827d61a6a091bf8
Shudi Wang <shudiwsh2009@gmail.com>
2015-11-17 21:24:58 +0800

    update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 echo "#update git"
-cd /mnt/appointment/git/Appointment/
+cd /mnt/appointment/git/Reservation_THXX/
 echo "git fetch..."
 git checkout master
 git fetch --all
@@ -16,14 +16,14 @@ echo "shutdown tomcat..."
 sleep 5
 echo "replace war..."
 rm -rf /mnt/appointment/apache-tomcat-8.0.26/webapps/appointment*
-cp /mnt/appointment/git/Appointment/target/appointment.war /mnt/appointment/apache-tomcat-8.0.26/webapps
+cp /mnt/appointment/git/Reservation_THXX/target/appointment.war /mnt/appointment/apache-tomcat-8.0.26/webapps
 echo "start tomcat..."
 /mnt/appointment/apache-tomcat-8.0.26/bin/startup.sh
 sleep 10
 
 echo "#deploy jar"
 echo "replace jar..."
-cp -f /mnt/appointment/git/Appointment/target/appointment.jar /mnt/appointment/reminder
+cp -f /mnt/appointment/git/Reservation_THXX/target/appointment.jar /mnt/appointment/reminder
 echo "restart cron"
 service cron restart
 
